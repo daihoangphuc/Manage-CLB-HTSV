89e8ca79ec7806b95599aa98a58d2d78f9bb5a73
phuc <nguyendaihoangphuc24@gmail.com>
2024-06-18 11:34:35 +0700

    Tets

--- obj/Debug/net6.0/Manage_CLB_HTSV.AssemblyInfo.cs
+++ obj/Debug/net6.0/Manage_CLB_HTSV.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Manage_CLB_HTSV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0df9cd030a786e448ecb15beb0a819706767e69c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+486595db01a56d454eeb742daeebf22e7927a54e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Manage_CLB_HTSV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Manage_CLB_HTSV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
